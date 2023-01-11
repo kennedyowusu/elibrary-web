89168b65caedbbfaa5d5179f2e161b128ea3120c
kennedyowusu <kennediowusu@gmail.com>
2023-01-11 23:58:43 +0000

    use navigate hook, add missing page

---
+++ .history/src/Pages/authentication/Registration_20230111234115.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import loginImage from '../../assets/library.png';
+import Button from '../../components/Button';
+import Form from '../../components/Form';
+
+const Registration = () => {
+ return ()
+);
+
+export default Registration;
---
+++ .history/src/components/NavButton_20230111232256.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const NavButton = ({ title, className }) => (
+  <button type='submit' className={className} onClick={onClick}>
+    {title}
+  </button>
+)
+
+export default NavButton;
