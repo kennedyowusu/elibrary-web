d7784e27c91259a302f607b20f3e5b1c076faa0f
kennedyowusu <kennediowusu@gmail.com>
2023-01-11 10:42:09 +0000

    place logo and hamburger icon side by side

---
+++ .history/src/components/Navbar_20230111102618.js
@@ -0,0 +1,88 @@
+// import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HiBars3 } from 'react-icons/hi2';
+import Logo from '../assets/library.png';
+import NavButton from './NavButton';
+
+const Navbar = () => {
+  const navLink = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'About',
+      link: '/about',
+    },
+    {
+      name: 'Dashboard',
+      link: '/dashboard',
+    },
+    {
+      name: 'Profile',
+      link: '/profile',
+    },
+
+  ];
+
+  // const [toggleNav, setToggleNav] = useState(false);
+
+  // const handleClick = () => {
+  //   setToggleNav(!toggleNav);
+  // };
+
+  return (
+    <nav className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:py-5 px-14">
+        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800 bg-red-900">
+          <img src={Logo} alt="Logo" className="w-10 h-10 ml-2 mr-3" />
+          <span className="text-[#317773]">Edu</span>
+          {' '}
+          Library
+          {' '}
+          <div
+          className="flex justify-end md:hidden lg:hidden cursor-pointer text-2xl text-gray-800 hover:text-[#317773] hover:duration-500"
+        >
+          <HiBars3 className="w-10" />
+        </div>
+        </div>
+
+        
+
+        <div className="flex items-center justify-end w-full">
+          <ul className="md:items-center gap-4 hidden md:flex lg:flex">
+            {navLink.map((link) => (
+              <li key={link.name} className="md:ml-8 text-xl">
+                <a
+                  href={link ? link.link : '/'}
+                  className="text-gray-800 font-medium text-lg hover:border-solid border-2 p-2 rounded-lg
+                border-transparent hover:border-[#ccc] hover:text-bold hover:duration-500
+                "
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+            <Link to="/login" className="ml-8 mr-4">
+              <NavButton
+                title="Login"
+                className="border-2 px-6 py-2 rounded-md mt-[-6px] font-bold hover:duration-500 hover:bg-[#317773] text-[#317773] hover:text-white"
+              />
+            </Link>
+
+            <Link to="register">
+              <NavButton
+                title="Register"
+                className="bg-[#317773] text-white px-8 py-2 rounded-md mt-[-6px] font-bold hover:duration-500 hover:bg-[#fff] hover:text-[#317773] border-2 border-[#317773]"
+              />
+            </Link>
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ .history/tailwind.config_20230111094312.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+       sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
