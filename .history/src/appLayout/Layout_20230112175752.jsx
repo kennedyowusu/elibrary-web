f95bf28b2cbd4b57a8b0dd26fccc81fdffc78eb3
kennedyowusu <kennediowusu@gmail.com>
2023-01-12 17:59:50 +0000

    relocate app layout file

---
+++ .history/src/appLayout/Layout_20230112175752.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Footer from '../components/footer/Footer'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
