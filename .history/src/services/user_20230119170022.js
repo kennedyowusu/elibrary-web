259588cebef91f7d575f1d157971ea212c20be2a
kennedyowusu <kennediowusu@gmail.com>
2023-01-19 17:00:36 +0000

    update endpoint

--- .history/src/services/user_20230119170022.js
+++ .history/src/services/user_20230119170022.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3000/api'
+const API_URL = 'http://localhost:3000/api/'
 
 const getPublicContent = () => {
   return axios.get(API_URL + 'all')
